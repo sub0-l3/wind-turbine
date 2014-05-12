b13e2cfc1e6a03a271e301506a8095601596d9be
subzero <subhrajit.debnath@gmail.com>
2014-05-12 22:52:57 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WindTurbine::Application.config.secret_token = '8c49d0f37170b0c6b9d054f027f2989e10509ee51c9179aed8741d05bfee42c6cae2ed9e52a053b51c108b7e3e89f9ff047b2b9224f843cb1cf5b30042f9cab5'
