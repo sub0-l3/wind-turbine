64868ca8f41b4f4fe6c0d15de90e4b7a246b297b
subzero <subhrajit.debnath@gmail.com>
2014-06-01 01:11:07 +0530

    Added capacity animation, UI/UX changes,seed file with AAER company's data

--- app/models/company_turbine.rb
+++ app/models/company_turbine.rb
@@ -1,5 +1,5 @@
 class CompanyTurbine < ActiveRecord::Base
   belongs_to :company
-  attr_accessible :turbine_model,:company_id,:cutin_velocity,:rated_power,:rated_velocity, :cutout_velocity
+  attr_accessible :turbine_model,:alpha,:beta,:company_id,:cutin_velocity,:rated_power,:rated_velocity, :cutout_velocity
 
 end
